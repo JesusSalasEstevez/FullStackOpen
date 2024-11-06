8e6e35ee54a7f133f96949578ff550f277d8acba
Jesús <jesussalasestevez@gmail.com>
2024-11-06 22:40:44 +0100

    1.1 done

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+const Button = ({text, handleClick}) => <button onClick={handleClick}>{text}</button>
+
+const Result = ({text, votes}) => <p>{text} {votes}</p>
+
+
+
+const App = () => {
+  const voteGood = () => {
+    const vote = good + 1
+    setGood(vote)
+  }
+  
+  const voteNeutral = () =>{
+    const vote = neutral + 1
+    setNeutral(vote)
+  }
+
+  const voteBad = () => {
+    const vote = bad + 1
+    setBad(vote)
+  }
+  
+  // guarda los clics de cada botón en su propio estado
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button text={'good'} handleClick={voteGood}/>
+      <Button text={'neutral'} handleClick={voteNeutral} />
+      <Button text={'bad'} handleClick={voteBad} />
+      <h1>statistics</h1>
+      <Result text={'good'} votes={good} />
+      <Result text={'neutral'} votes={neutral} />
+      <Result text={'bad'} votes={bad} />
+
+    </div>
+  )
+}
+
+export default App

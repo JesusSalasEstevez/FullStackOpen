eff07751fa9dc2b3a2bf2442ebd294b51d28dc77
JesusSalasEstevez <jesussalas@gmail.com>
2024-11-06 14:19:54 +0100

    changes on parts done

--- part1/1excercise/src/App.jsx
+++ part1/1excercise/src/App.jsx
@@ -6,16 +6,16 @@ const Header = (props) => {
 
 const Part = (props) => {
   return(
-    <p>{props.part} {props.exercises}</p>
+    <p>{props.part} {props.exercise}</p>
   )
 }
 
 const Content = (props) => {
   return(
     <div>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
+      <Part part={props.part1} exercise={props.exercise1} />
+      <Part part={props.part2} exercise={props.exercise2} />
+      <Part part={props.part3} exercise={props.exercise3} />
     </div>
   )
 }

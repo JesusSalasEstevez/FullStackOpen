b36c4df0f6af0bbbfd3d65f113fce00845c772a4
JesusSalasEstevez <jesussalas@gmail.com>
2024-11-06 11:51:58 +0100

    App changed adding a new component at App.jsx

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,16 +1,18 @@
-const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log (now, a+b)
-  console.log(now, a+b)
-
+const Hello = () => {
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a+b}
-      </p>
+      <p>Hellow world</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return(
+    <div>
+      <h1>Greeting</h1>
+      <Hello />
+      <Hello />
+      <Hello />
     </div>
   )
 }

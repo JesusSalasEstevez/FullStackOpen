0d2fdd81fda08b0b3d88adc9dbc597602a4ac196
JesusSalasEstevez <jesussalas@gmail.com>
2024-11-08 14:25:28 +0100

    2.11 done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({filter, searchInfo}) => {
   return(
@@ -32,6 +33,13 @@ const Persons = ({contacts}) => {
 
 const App = () => {
 
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setNewContacts(response.data)
+      })
+  }, [])
 
   const addName = (event) => {
     event.preventDefault()
@@ -51,12 +59,7 @@ const App = () => {
     setNewContacts(personsToShow)
   }
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setNewFilter] = useState('')

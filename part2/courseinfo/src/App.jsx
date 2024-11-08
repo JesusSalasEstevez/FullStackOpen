52b2a8aec9cdefa73c5591067d5427b9788b6413
JesusSalasEstevez <jesussalas@gmail.com>
2024-11-08 10:57:51 +0100

    2.7 done

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,33 +1,4 @@
-const Header = ({header}) => {
-  return(
-    <h1>{header}</h1>
-  )
-}
-
-const Part = ({part}) => {
-  return (
-    <p>{part.name} {part.exercises}</p>
-  )
-}
-
-const Content = ({parts}) => {
-  const total = parts.reduce((s, part) => s += part.exercises, 0)
-  return(
-    <>
-      {parts.map(part => <Part key={part.id} part={part}/>)}
-      <p>total of {total} exercises</p>
-    </>
-  )
-}
-
-const Course = ({course}) =>{
-  return (
-    <>
-      <Header header={course.name} />
-      <Content parts={course.parts} />
-    </>
-  )
-}
+import Course from "./components/Course"
 
 const App = () => {
   const courses = [
---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+
+const App = () => {
+
+  const input = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+    if(persons.find(p => p.name == newName)){
+      window.alert(`${newName} is already added to phonebook`)
+    }else{
+      const newPerson ={name: newName}
+      const newPersons = persons.concat(newPerson)
+      setNewName('')
+      setPersons(newPersons)
+    }
+  }
+
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={input}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+    </div>
+  )
+}
+
+export default App

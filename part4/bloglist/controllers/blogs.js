ff7783aa1a208701534611b16bd6dbec30d38eba
JesusSalasEstevez <jesussalasestevez@gmail.com>
2024-11-29 10:08:58 +0100

    4.20 done

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -4,14 +4,6 @@ const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 require('express-async-errors')
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.startsWith('Bearer ')) {
-    return authorization.replace('Bearer ', '')
-  }
-  return null
-}
-
 blogsRouter.get('/', async (request, response, next) => {
   const blogs = await Blog.find({}).populate('user')
   response.status(200).json(blogs)
@@ -19,10 +11,11 @@ blogsRouter.get('/', async (request, response, next) => {
   
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
-  }
+  
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  console.log(decodedToken)
+
+  
   const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
